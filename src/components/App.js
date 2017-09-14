2b3df5f655e7f0e0a368f450141c7fed1cdfb1bd
sori-kang <sori.kang@rea-group.com>
2017-09-14 23:12:09 +1000

    feat: Add a link to create a post
    - use BrowerRoute, Route and Link to navigate pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom'
 import { fetchCategories, fetchAllPosts } from '../utils/api'
 import { sortByVoteScore, sortByTimestamp } from '../utils/helpers'
 
@@ -30,27 +31,51 @@ class App extends Component {
 
     return (
       <div>
-        <h1>Readable</h1>
-        <h2>Categories</h2>
-        <lo>
-          {categories.map(category => (
-            <li key={category.name}>{category.name}</li>
-          ))}
-        </lo>
-        <h2>Posts</h2>
-        <button onClick={() => {this.setState({isSortbyVoteScore: false})}}>
-          latest
-        </button>
-        <button onClick={() => {this.setState({isSortbyVoteScore: true})}}>
-          highest score
-        </button>
-        <lu>
-          {sortedPosts.map(post => (
-            <li key={post.id}>
-              {post.title}
-            </li>
-          ))}
-        </lu>
+        <Route exact path='/' render={() => (
+          <div>
+            <div>
+              <h1>Readable</h1>
+            </div>
+            <div>
+              <h2>Categories</h2>
+              <lo>
+                {categories.map(category => (
+                  <li key={category.name}>{category.name}</li>
+                ))}
+              </lo>
+            </div>
+
+            <div>
+              <h2>Posts</h2>
+              <button onClick={() => {this.setState({isSortbyVoteScore: false})}}>
+                latest
+              </button>
+              <button onClick={() => {this.setState({isSortbyVoteScore: true})}}>
+                highest score
+              </button>
+              <lu>
+                {sortedPosts.map(post => (
+                  <li key={post.id}>
+                    {post.title}
+                  </li>
+                ))}
+              </lu>
+            </div>
+
+            <div>
+              <Link to='/create'>Create a post</Link>
+            </div>
+          </div>
+        )}>
+        </Route>
+
+        <Route exact path='/create' render={() => (
+          <div>Hello</div>
+        )}>
+        </Route>
+
+
+
       </div>
     );
   }
