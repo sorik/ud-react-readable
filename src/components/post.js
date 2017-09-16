8f32733164b0d57e466bee787cec90fe35d83faa
sori-kang <sori.kang@rea-group.com>
2017-09-16 16:43:12 +1000

    feat: display details of post

--- src/components/post.js
+++ src/components/post.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import moment from 'moment'
+
+const TIME_FORMAT = 'DD-MM-YYYY HH:mm:ss'
 
 class Post extends Component {
   render() {
-    const { title } = this.props.post
+    const { id, title, author, timestamp, body, voteScore } = this.props.post
+    const timeString = moment(timestamp).format(TIME_FORMAT)
 
     return (
       <div>
-        <div>{title}</div>
+        <div>
+          <div>
+            <h2>{title}</h2>
+          </div>
+          <div>
+            <h4>Author: <span>{author}</span></h4>
+          </div>
+          <div>
+            <h5>wrote at: <span>{timeString}</span></h5>
+          </div>
+          <div>
+            <h5>vote score: <span>{voteScore}</span></h5>
+          </div>
+          <div>
+            {body}
+          </div>
+        </div>
+        <div>
+          <div>
+            <Link to={'/edit/' + id}>Edit</Link>
+          </div>
+        </div>
+
       </div>
     )
   }
@@ -16,8 +42,6 @@ class Post extends Component {
 function mapStateToProp(state, props) {
   const postId = props.match.params.id
   const posts =  state.posts.filter(post => post.id === postId)
-
-  console.log(posts)
 
   return posts.length > 0 ? { post: posts[0] } : { post: {} }
 }
