103feab92f50210d6a171ca002254d98daea4cae
sori-kang <sori.kang@rea-group.com>
2017-09-21 23:54:52 +1000

    fix: not displaying anything when the category on the route is invalid

--- src/components/categoryView.js
+++ src/components/categoryView.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react'
 import PostList from './postList'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class CategoryView extends Component {
   render() {
+    const { categories } = this.props
     const { category } = this.props.match.params
+
     return (
       <div>
-        <div>
-          <h1>{category}</h1>
-        </div>
-        <div>
-          <PostList category={category}/>
-        </div>
-        <br/><br/>
-        <div>
-          <Link to='/'>Go to main</Link>
-        </div>
+        {categories.indexOf(category) > 0 && (
+          <div>
+            <div>
+              <h1>{category}</h1>
+            </div>
+            <div>
+              <PostList category={category}/>
+            </div>
+            <br/><br/>
+            <div>
+              <Link to='/'>Go to main</Link>
+            </div>
+          </div>
+        )}
       </div>
     )
   }
 }
 
-export default CategoryView
+function mapStateToProp(state) {
+  return state.category
+}
+export default connect(mapStateToProp)(CategoryView)
